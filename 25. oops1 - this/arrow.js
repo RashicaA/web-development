5d8068b5c83e0aeece939ff1436a56641406bfb4
Kanishk Agrawal <kanishk71@gmail.com>
2025-04-04 07:45:33 +0530

    Arrow functions - this

--- 25. oops1 - this/arrow.js
+++ 25. oops1 - this/arrow.js
@@ -1,15 +1,51 @@
-function add(a, b) {
-    console.log("hello")
-    return a + b;
+// function add(a, b) {
+//     console.log("hello")
+//     return a + b;
+// }
+// console.log(add(1, 2))
+
+// const add2 = (a, b) => a + b;
+
+// console.log(add2(1, 2))
+
+// const add3 = (a, b) => {
+//     console.log("hello");
+//     return a + b;
+// }
+// console.log(add3(1, 2))
+
+
+// // this in arrow functions
+
+// const person = {
+//     name: 'john doe',
+//     arrow: () => console.log(`arrow: ${this.name}`),
+//     fn: function () {
+//         console.log(`regular: ${this.name}`)
+//     }
+// }
+
+// console.log(person.fn()) // john
+// console.log(person.arrow()) // undefined
+
+
+// usefulness of this in arrow.
+
+const person2 = {
+    name: 'jane',
+    eventuallySayName: function () {
+        function actuallySayName() {
+            console.log(`first: ${this.name}`); // undefined, since in nested func, this is global/window.
+        }
+
+        const actuallySayNameArrow = () => {
+            console.log(`second: ${this.name}`) // jane
+        }
+
+        actuallySayName();
+        actuallySayNameArrow();
+    },
 }
-console.log(add(1, 2))
 
-const add2 = (a, b) => a + b;
+person2.eventuallySayName();
 
-console.log(add2(1, 2))
-
-const add3 = (a, b) => {
-    console.log("hello");
-    return a + b;
-}
-console.log(add3(1, 2))
