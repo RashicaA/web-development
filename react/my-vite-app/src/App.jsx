d61181e70e4e7f33506d6c5ed9065a97b589800d
Kanishk Agrawal <kanishk71@gmail.com>
2025-04-25 08:11:37 +0530

    react vite & cra setup

---
+++ react/my-vite-app/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HelloComponent from './HelloComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <HelloComponent />
+        <HelloComponent />
+        <HelloComponent />
+        <HelloComponent />
+        <HelloComponent />
+      </div>
+      <h1>Vite + React !!!!</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ react/my-vite-app/src/HelloComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HelloComponent = () => {
+    return <h1>Hello, my name is John.</h1>
+}
+
+export default HelloComponent;
