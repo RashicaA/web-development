753645a65d8640d7658e9fed171a251f29394f10
Kanishk Agrawal <kanishk71@gmail.com>
2025-04-28 09:02:36 +0530

    Todos

--- react/my-vite-app/src/TodoList/Todos.jsx
+++ react/my-vite-app/src/TodoList/Todos.jsx
@@ -12,10 +12,30 @@ const ToDoList = () => {
     const [input, setInput] = useState("");
 
     const addTask = () => {
+        // tasks.push(input); NOT WORK.
+
+        // const arr = [...tasks, input];
+        // setTasks(arr);
+        // OR
+
+        setTasks(tasks.concat(input))
+        setInput("")
     };
 
-    const removeTask = () => {
+    const removeTask = (idx) => {
+        // delete item on index = idx from todos array.
+
+        console.log(idx);
+
+        const newTasks = tasks.filter((task, i) => i !== idx);
+
+        setTasks(newTasks)
+
     };
+
+    const handleInputChange = (event) => {
+        setInput(event.target.value)
+    }
 
     return (
         <div style={{ maxWidth: "400px", margin: "auto", textAlign: "center" }}>
@@ -25,13 +45,20 @@ const ToDoList = () => {
                 value={input}
                 placeholder="Enter task..."
                 style={{ marginRight: "10px", padding: "10px" }}
+                onChange={handleInputChange}
             />
-            <button>Add Task</button>
+            <button onClick={addTask}>Add Task</button>
 
             <ul>
-                Display tasks here
+                Display tasks here:
+                {tasks.map((task, index) => (
+                    <li key={index + 'task'}>
+                        {task}
+                        <button onClick={() => removeTask(index)} style={{ color: 'red', marginLeft: "10px" }}> X</button>
+                    </li>
+                ))}
             </ul>
-        </div>
+        </div >
     );
 };
 
