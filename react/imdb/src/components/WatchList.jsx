0d67fa90ad96f322f72350cb47662b33c56e8e7f
Kanishk Agrawal <kanishk71@gmail.com>
2025-05-12 08:33:30 +0530

    Sorting

--- react/imdb/src/components/WatchList.jsx
+++ react/imdb/src/components/WatchList.jsx
@@ -20,6 +20,23 @@ const WatchList = () => {
         }
     }, [])
 
+    const handleAscRatings = () => {
+        const sorted = watchListMovies.sort((a, b) => a.vote_average - b.vote_average);
+        console.log(watchListMovies)
+        // to create a copy of array, since .sort returns same reference of memory
+        setWatchListMovies([...sorted])
+    }
+
+    const handleDescRatings = () => {
+        const sorted = watchListMovies.sort((a, b) => b.vote_average - a.vote_average)
+        // to create a copy of array, since .sort returns same reference of memory
+        setWatchListMovies(sorted.slice())
+
+    }
+    if (watchListMovies.length === 0) {
+        return <p className='m-4'>Please add movies to watchlist.</p>
+    }
+
     return (
         <div className='border border-gray-400 rounded-lg m-5 overflow-hidden'>
             <table className='w-full bg-white border-collapse text-left text-sm'>
@@ -29,7 +46,9 @@ const WatchList = () => {
                         <th className='font-medium text-gray-900'>
                             <div className='flex'>
                                 <div>
+                                    <i className='fa-solid fa-arrow-up cursor-pointer p-1' onClick={handleAscRatings}></i>
                                     Ratings
+                                    <i className='fa-solid fa-arrow-down cursor-pointer p-1' onClick={handleDescRatings}></i>
                                 </div>
                             </div>
                         </th>
@@ -53,14 +72,15 @@ const WatchList = () => {
                                     <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt='' className='h-[5rem] w-[8rem] object-fit'></img>
                                     <div className='text-sm font-medium pl-1'>{movie.title}</div>
                                 </td>
-                                <td className=' py-4' >{movie.vote_average}</td>
+                                <td className=' py-4' >
+
+                                    {movie.vote_average}
+                                </td>
                                 <td className=' py-4' >{movie.popularity}</td>
                                 <td className=' py-4' >{getGenreFromGenreIds(movie.genre_ids)}</td>
                             </tr>
                         )
                     })}
-
-
                 </tbody>
             </table>
         </div>
